8e317e2683450166a9bf65338e2e21b21c885bd1
Pawel <kuriatapaw@gmail.com>
2019-11-16 13:16:42 +0100

    feat: tests

--- PEA/PEA/PEA.cpp
+++ PEA/PEA/PEA.cpp
@@ -4,10 +4,13 @@
 #include "loadFromFile.h"
 #include "inputByHand.h"
 #include "randomValues.h"
+#include "graph.h"
+#include <chrono>
+#include <fstream>
 using namespace std;
 
 void mainMenu() {
-	string menu = "1. Wczytaj z pliku\n2. Generuj losowo\n3. Wprowadz recznie\n4. Wyjdz";
+	string menu = "1. Wczytaj z pliku\n2. Generuj losowo\n3. Wprowadz recznie\n4. Wyjdz\n5. Test";
 
 	bool exit = false;
 	while (!exit) {
@@ -26,6 +29,62 @@ void mainMenu() {
 				break;
 			case '4':
 				exit = true;
+				break;
+			case '5':
+				const int maxNrOfPoints = 10;
+				const int reps = 10;
+
+				ofstream file;
+				file.open("test.csv");
+				file << "Nr of Points;Dynamic Programming results\n";
+
+				for (int i = 3; i <= maxNrOfPoints; i++) {
+					file << i << ";";
+					for (int j = 0; j < reps; j++) {
+						Graph graf = graphTest(i);
+						auto t1 = chrono::high_resolution_clock::now();
+						graf.dynamicProgramming();
+						auto t2 = chrono::high_resolution_clock::now();
+						auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+						file << duration << ";";
+					}
+					file << "\n";
+				}
+
+
+				file << "\n\n\nNr of Points;Branch and Bound results\n";
+
+				for (int i = 3; i <= maxNrOfPoints; i++) {
+					file << i << ";";
+					for (int j = 0; j < reps; j++) {
+						Graph graf = graphTest(i);
+						auto t1 = chrono::high_resolution_clock::now();
+						graf.branchAndBound();
+						auto t2 = chrono::high_resolution_clock::now();
+						auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+						file << duration << ";";
+					}
+					file << "\n";
+				}
+
+				file << "\n\n\nNr of Points;Brute Force results\n";
+
+				for (int i = 3; i <= maxNrOfPoints; i++) {
+					file << i << ";";
+					for (int j = 0; j < reps; j++) {
+						Graph graf = graphTest(i);
+						auto t1 = chrono::high_resolution_clock::now();
+						graf.bruteForce();
+						auto t2 = chrono::high_resolution_clock::now();
+						auto duration = chrono::duration_cast<chrono::microseconds>(t2 - t1).count();
+						file << duration << ";";
+					}
+					file << "\n";
+				}
+				
+				cout << endl << "Koniec...";
+				file.close();
+				_getch();
 				break;
 		}
 	}
--- PEA/PEA/randomValues.cpp
+++ PEA/PEA/randomValues.cpp
@@ -26,3 +26,28 @@ void randomValuesMenu(int num) {
 	menu(graf);
 	delete graf;
 }
+
+Graph graphTest(int num) {
+
+
+
+	random_device rd;
+	mt19937 eng(rd());
+
+	int nrOfPoints = num;
+	Graph* graf = new Graph(nrOfPoints, "name");
+	uniform_int_distribution<int> di(1, 100);
+	int cur;
+	for (int i = 0; i < nrOfPoints; i++) {
+		for (int j = 0; j < nrOfPoints; j++) {
+			if (i != j) {
+				cur = di(eng);
+				graf->insertNumber(i, j, cur);
+			}
+		}
+	}
+
+	Graph res = *graf;
+	delete graf;
+	return res;
+}
